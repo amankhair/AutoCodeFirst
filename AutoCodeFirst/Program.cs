8c61ae67d348a7a46229b1fd3def3a160fd315e3
КаирбаевА <@403_5_>
2018-02-01 17:42:29 +0600

    Add project files.

---
+++ AutoCodeFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mcsEntity db = new mcsEntity();
+            db.Database.Log =
+                (s => System.Diagnostics.Debug.WriteLine(s));
+
+            //List<TrackEvaluationComment> com = new List<TrackEvaluationComment>
+
+            //List<TrackEvaluation> evalution = db.TrackEvaluation.Include(inc => inc.);
+
+            //yavnaya zagruzka
+            
+        }
+    }
+}
